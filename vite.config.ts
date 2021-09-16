00386e5b034f681508cbfe8ba10cb0720a700edb
Yannick Küchlin <y.kuechlin@taifun-tofu.de>
2021-09-16 13:41:10 +0200

    bundler changed to vite-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    visualizer({
+      filename: 'stats.html',
+      template: 'treemap',
+      title: 'run-ts stats',
+    }),
+  ],
   define: {
     'process.env': process.env,
   },
